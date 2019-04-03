e67210f138c16727183b13e2a30381d0a23dee9f
dlonghi <30272376+dlonghi@users.noreply.github.com>
2019-04-03 17:18:42 -0300

    Update sql-updates-gerador.sh

--- sql-updates-gerador.sh
+++ sql-updates-gerador.sh
@@ -1,1 +1,1 @@
-IFS=$'\n'; for l in `cat arquivo.txt | egrep -v '^#|^$'`; do echo $l | awk '{print "UPDATE db.table SET X = "$1", Y=" $2 " WHERE Z=\x27" $3 "\x27;"}' ; done
+IFS=$'\n'; for l in `cat arquivo.txt | egrep -v '^#|^$'`; do echo $l | awk '{print "UPDATE db.table SET a,b VALUES (\x27" $1 "\x27, \x27" $2 "\x27) WHERE c=\x27" $3 "\x27;"}' ; done
